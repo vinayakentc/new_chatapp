602fb998f63bd8fa0c55c8feec3abc574c468d03
vinayakentc <66103397+vinayakentc@users.noreply.github.com>
2021-06-27 23:51:50 +0530

    Update and rename file1.txt.txt to start.sh

---
+++ scripts/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/ubuntu/Backend/new_chatapp/
+
+docker build -t backendimg_1 .
+
+docker run --name mysqldb -d databaseimg_2 
+
+docker run --name backend --link mysqldb -d backendimg_1 
+
+docker run --name frontend --link backend -p 80:80 -d frontendimg_1 
+
