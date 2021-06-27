caf4f1224b111036b494f0c224ab1629921d45d2
vinayakentc <66103397+vinayakentc@users.noreply.github.com>
2021-06-28 01:08:59 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,9 @@ cd /home/ubuntu/Backend/new_chatapp/
 
 docker build -t backendimg_1 .
 
+docker login 
+
+
 docker run -v mysqlvol1:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=ganesh123 -e MYSQL_USER=admin --name mysqldb -d databaseimg_2 
 
 docker run --name backend --link mysqldb -d backendimg_1 
