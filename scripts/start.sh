126fbc9fd2a19252f6952aa0b429b2cbe23dd3ee
vinayakentc <66103397+vinayakentc@users.noreply.github.com>
2021-06-28 01:20:04 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,7 +14,7 @@ docker build -t  databaseimg_2 .
 docker login 
 
 
-docker run -v mysqlvol1:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=ganesh123 -e MYSQL_USER=admin --name mysqldb -d databaseimg_2 
+docker run -d -v mysqldata:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=ganesh123 -e MYSQL_USER=admin --name mysqldb  databaseimg_2 
 
 docker run --name backend --link mysqldb -d backendimg_1 
 
