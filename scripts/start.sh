d8aec63ea8e91085751d40ee71c75ed8e3453b38
vinayakentc <66103397+vinayakentc@users.noreply.github.com>
2021-06-28 13:54:15 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,7 +16,7 @@ docker login
 
 docker run -d -v mysqldata:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=ganesh123 -e MYSQL_USER=admin --name mysqldb  databaseimg_2 
 
-docker run --name backend --link mysqldb --memory=2g--memory-reservation=1550m -d backendimg_1 
+docker run --name backend --link mysqldb   --memory=2g   --memory-reservation=1550m   -d backendimg_1 
 
-docker run --name frontend --link backend -p 80:80 --cpus=0.30 -d frontendimg_1 
+docker run --name frontend --link backend -p 80:80   --cpus=0.30   -d frontendimg_1 
 
